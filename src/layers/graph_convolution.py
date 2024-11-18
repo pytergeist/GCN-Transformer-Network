21ea8813f87483274b2e3e73520615841cb9f294
pytergeist <tompopeworks@gmail.com>
2024-11-18 19:27:14 +0000

    Adding graph convolution layer

---
+++ src/layers/graph_convolution.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+class GraphConvolutionLayer(tf.keras.layers.Layer):
+    def __init__(self, output_dim):
+        super(GraphConvolutionLayer, self).__init__()
+        self.output_dim = output_dim
+        self.dense = tf.keras.layers.Dense(output_dim, activation=None)
+
+    def call(self, adjacency_matrix, node_features):
+        degree_matrix_inv_sqrt = tf.linalg.diag(1.0 / tf.sqrt(tf.reduce_sum(adjacency_matrix, axis=-1)))
+        normalized_adj = tf.matmul(tf.matmul(degree_matrix_inv_sqrt, adjacency_matrix), degree_matrix_inv_sqrt)
+        aggregated_features = tf.matmul(normalized_adj, node_features)
+        return tf.nn.relu(self.dense(aggregated_features))
